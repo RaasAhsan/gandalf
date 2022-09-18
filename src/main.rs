df54b07f09d1593226f51ab867215b5efa264f78
Raas Ahsan <raas.ahsan@gmail.com>
2022-09-17 23:54:31 -0500

    Example object logic

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use gandalf::{
     interpreter::Environment,
-    lf::{Family, FamilyName, Kind, Signature, TermName},
+    lf::{Family, FamilyName, Kind, TermName},
 };
 
 fn main() {
@@ -8,13 +8,19 @@ fn main() {
     let nat_family = FamilyName::new("nat");
     let z_term = TermName::new("z");
     let s_term = TermName::new("s");
-    environment.declare_family(&nat_family, &Kind::Type);
-    environment.declare_term(&z_term, Family::Const(nat_family.clone()));
-    environment.declare_term(
-        &s_term,
-        Family::Abs(
-            Box::new(Family::Const(nat_family.clone())),
-            Box::new(Family::Const(nat_family.clone())),
-        ),
-    );
+    environment
+        .declare_family(&nat_family, &Kind::Type)
+        .unwrap();
+    environment
+        .declare_term(&z_term, &Family::Const(nat_family.clone()))
+        .unwrap();
+    environment
+        .declare_term(
+            &s_term,
+            &Family::Abs(
+                Box::new(Family::Const(nat_family.clone())),
+                Box::new(Family::Const(nat_family.clone())),
+            ),
+        )
+        .unwrap();
 }
